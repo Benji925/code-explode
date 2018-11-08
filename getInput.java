39904326df44da07a86ea4499facf277517fb962
Charlie Atchinson <atchinson.charlie@gmail.com>
2018-11-07 22:55:18 -0500

    redoes the whole damn thing
    Just....ugh. what I had before was not fucking working my dudes

--- getInput.java
+++ getInput.java
@@ -1,45 +1,55 @@
+import java.time.DayOfWeek;
 import java.util.Arrays;
 
 public class GetInput {
-    public static String[] getInput() {
+    public static void main(String [] args) {
         System.out.println("Hello User! This is the Code Explode Daily Planning App, or CEDPA");
         boolean keepGoing = true;
-        //this boolean will be used to ask if the user has more classes to enter
+        String[] classes = new String[10];
         int i = 0;
-        //this int is for keeping track of the number of classes and for storing them in the array
-        String[] className = new String[10];
-        //this array is for keeping track of the number of classes
-        while (keepGoing) {
-            if (i == 0) {
-                System.out.println("Please input the name of your first class.");
-            } else {
-                System.out.println("Please input your next class");
-            }
-            className[i] = TextIO.getWord();
-            boolean moreDays = true;
-            //this boolean is to ask if the class meets more than one day a week
-            String[] daysOfWeek = new String[7];
-            //this array keeps track of the days of the week, kind of. I need a better way of doing this
-            int j = 0;
-            //this int keeps track of how many days a week the class meets
-            while (moreDays) {
-                System.out.println("Please enter the day of the week this class meets:");
-                daysOfWeek[j] = TextIO.getWord();
-                System.out.println("What time does the class meet on?");
-                System.out.println("Does the class meet another day?");
-                moreDays = TextIO.getBoolean();
-                j++;
-            }
+        boolean[] daysOfWeek = new boolean[7];
+        while(keepGoing) {
+            getClasses(classes[], i);
+            days(daysOfWeek[]);
 
             System.out.println("Do you have another class?");
             keepGoing = TextIO.getBoolean();
-            i++;
         }
-        return className;
     }
 
-    public static void main(String[] args) {
-        System.out.println(Arrays.toString(getInput()));
+    public static String[] getClasses (String[] classes, int i) {
+        System.out.println("What is the name of your class?");
+        classes[i] = TextIO.getlnString();
+        return classes[]
     }
-    //sees what kind of information the user input
+    public static boolean[] days (boolean[] daysOfWeek){
+        boolean anotherDay = true;
+        while(anotherDay){
+            System.out.println("What day does the class meet? (enter the three letter abbreviation, ex thu.)");
+            String day = TextIO.getWord();
+            switch (day) {
+                case: "sun"
+                    daysOfWeek[0]=true; break;
+                case: "mon"
+                    daysOfWeek[1]= true; break;
+                case: "tue"
+                    daysOfWeek[2]= true; break;
+                case: "wed"
+                    daysOfWeek[3]= true; break;
+                case: "thu"
+                    daysOfWeek[4]= true; break;
+                case: "fri"
+                    daysOfWeek[5]= true; break;
+                case: "sat"
+                    daysOfWeek[6]= true; break;
+                    default:
+                        System.out.println("You made an error in entering the day");
+            }
+            System.out.println("Does the class meet another day of the week?");
+            anotherDay = TextIO.getBoolean();
+        }
+        return daysOfWeek[]
+    }
+    public static
+
 }
