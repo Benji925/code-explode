9f8c6aaab69f6b83ab0f4b1c1e0302542b868a5e
Charlie Atchinson <atchinson.charlie@gmail.com>
2018-11-08 10:20:02 -0500

    adds methods start and duration, changes the return of getClasses to a String
    So it will compile and begin to run but then will not let the user input a second class and I can't figure out why. If you folks can, that would be great.

--- getInput.java
+++ getInput.java
@@ -9,47 +9,64 @@ public class GetInput {
         int i = 0;
         boolean[] daysOfWeek = new boolean[7];
         while(keepGoing) {
-            getClasses(classes[], i);
-            days(daysOfWeek[]);
-
+            classes[i] = getClasses();
+            days(daysOfWeek);
+            double duration = durationOfClass();
+            int start = startTime();
             System.out.println("Do you have another class?");
             keepGoing = TextIO.getBoolean();
+            i++;
         }
     }
 
-    public static String[] getClasses (String[] classes, int i) {
+    public static String getClasses () {
         System.out.println("What is the name of your class?");
-        classes[i] = TextIO.getlnString();
-        return classes[]
+        String schoolClass = TextIO.getlnString();
+        return schoolClass;
     }
+
     public static boolean[] days (boolean[] daysOfWeek){
         boolean anotherDay = true;
         while(anotherDay){
             System.out.println("What day does the class meet? (enter the three letter abbreviation, ex thu.)");
             String day = TextIO.getWord();
             switch (day) {
-                case: "sun"
+                case "sun":
                     daysOfWeek[0]=true; break;
-                case: "mon"
+                case "mon":
                     daysOfWeek[1]= true; break;
-                case: "tue"
+                case "tue":
                     daysOfWeek[2]= true; break;
-                case: "wed"
+                case "wed":
                     daysOfWeek[3]= true; break;
-                case: "thu"
+                case "thu":
                     daysOfWeek[4]= true; break;
-                case: "fri"
+                case "fri":
                     daysOfWeek[5]= true; break;
-                case: "sat"
+                case "sat":
                     daysOfWeek[6]= true; break;
-                    default:
-                        System.out.println("You made an error in entering the day");
+                default:
+                    System.out.println("You made an error in entering the day (try mon, tue, wed...)");
             }
             System.out.println("Does the class meet another day of the week?");
             anotherDay = TextIO.getBoolean();
         }
-        return daysOfWeek[]
+        return daysOfWeek;
     }
-    public static
+
+    public static double durationOfClass() {
+        System.out.println("How many hours does the class last?");
+        double duration = TextIO.getDouble();
+        return duration;
+    }
+
+    public static int startTime() {
+        System.out.println("What time does the class start?");
+        int startHour = TextIO.getInt();
+        TextIO.getChar();
+        int startMinute = TextIO.getInt();
+        int start = (startHour * 100) + startMinute;
+        return start;
+    }
 
 }
