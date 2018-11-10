9ce97103ba2985eac2a6609dfdd2bf268112fd69
Charlie Atchinson <atchinson.charlie@gmail.com>
2018-11-10 16:47:58 -0500

    Got it to work
    I pretty much rewrote like half of it, I can't list everything I did. But it compiles and runs and I am going to fix the rest of it tomorrow.

--- getInput.java
+++ getInput.java
@@ -10,7 +10,32 @@ public class GetInput {
         boolean[] daysOfWeek = new boolean[7];
         while(keepGoing) {
             classes[i] = getClasses();
-            days(daysOfWeek);
+            boolean meetAgain = true;
+            while (meetAgain){
+                String day = AskDay();
+                if(day.equals("sun")) {
+                    String[] Sunday = PutInSunday(classes, i);
+                    meetAgain = AnotherDay();
+                }else if(day.equals("mon")){
+                    String[] Monday = PutInMonday(classes, i);
+                    meetAgain = AnotherDay();
+                }else if(day.equals("tue")){
+                    String[] Tuesday = PutInTuesday(classes, i);
+                    meetAgain = AnotherDay();
+                }else if(day.equals("wed")){
+                    String[] Wednesday = PutInWednesday(classes, i);
+                    meetAgain = AnotherDay();
+                }else if(day.equals("thu")){
+                    String[] Thursday = PutInThursday(classes, i);
+                    meetAgain = AnotherDay();
+                }else if(day.equals("fri")){
+                    String[] Friday = PutInFriday(classes, i);
+                    meetAgain = AnotherDay();
+                }else if(day.equals("sat")){
+                    String[] Saturday = PutInSaturday(classes, i);
+                    meetAgain = AnotherDay();
+                }
+            }
             double duration = durationOfClass();
             int start = startTime();
             System.out.println("Do you have another class?");
@@ -25,45 +50,69 @@ public class GetInput {
         return schoolClass;
     }
 
-    public static boolean[] days (boolean[] daysOfWeek){
-        boolean anotherDay = true;
-        while(anotherDay){
-            boolean right = false;
-            while(!right){
-                System.out.println("What day does the class meet? (enter the three letter abbreviation, ex thu.)");
-                String day = TextIO.getWord();
-                switch (day) {
-                    case "sun":
-                        daysOfWeek[0]=true;
-                        right = true; break;
-                    case "mon":
-                        daysOfWeek[1]= true;
-                        right = true; break;
-                    case "tue":
-                        daysOfWeek[2]= true;
-                        right = true; break;
-                    case "wed":
-                        daysOfWeek[3]= true;
-                        right = true; break;
-                    case "thu":
-                        daysOfWeek[4]= true;
-                        right = true; break;
-                    case "fri":
-                        daysOfWeek[5]= true;
-                        right = true; break;
-                    case "sat":
-                        daysOfWeek[6]= true;
-                        right = true; break;
-                    default:
-                        System.out.println("You made an error in entering the day (try mon, tue, wed...)");
-                        right = false;
-                }
+    public static String AskDay(){
+        boolean right = false;
+        String day = " ";
+        while(!right){
+            System.out.println("What day does the class meet? (enter the three letter abbreviation, ex thu.)");
+            String check = TextIO.getWord();
+            if (check.equals("sun") || check.equals("mon")||check.equals("tue")||check.equals("wed")||check.equals("thu")||check.equals("fri")||check.equals("sat")) {
+                day = check;
+                right = true;
+            }else {
+                System.out.println("You made an error in entering the day (try mon, tue, wed...)");
+                right = false;
             }
-
-            System.out.println("Does the class meet another day of the week?");
-            anotherDay = TextIO.getBoolean();
         }
-        return daysOfWeek;
+        return day;
+    }
+
+    public static boolean AnotherDay(){
+        System.out.println("Does the class meet another day?");
+        boolean meetAgain = TextIO.getlnBoolean();
+        return meetAgain;
+    }
+
+    public static String[] PutInSunday(String[] classes, int i) {
+        String[] Sunday = new String [10];
+        Sunday[i] = classes[i];
+        return Sunday;
+    }
+
+    public static String[] PutInMonday(String[] classes, int i) {
+        String[] Monday = new String [10];
+        Monday[i] = classes[i];
+        return Monday;
+    }
+
+    public static String[] PutInTuesday(String[] classes, int i) {
+        String[] Tuesday = new String [10];
+        Tuesday[i] = classes[i];
+        return Tuesday;
+    }
+
+    public static String[] PutInWednesday(String[] classes, int i) {
+        String[] Wednesday = new String [10];
+        Wednesday[i] = classes[i];
+        return Wednesday;
+    }
+
+    public static String[] PutInThursday(String[] classes, int i) {
+        String[] Thursday = new String [10];
+        Thursday[i] = classes[i];
+        return Thursday;
+    }
+
+    public static String[] PutInFriday(String[] classes, int i) {
+        String[] Friday = new String [10];
+        Friday[i] = classes[i];
+        return Friday;
+    }
+
+    public static String[] PutInSaturday(String[] classes, int i) {
+        String[] Saturday = new String [10];
+        Saturday[i] = classes[i];
+        return Saturday;
     }
 
     public static double durationOfClass() {
