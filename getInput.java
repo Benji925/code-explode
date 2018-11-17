e90b9d33fdedf626c07bb0af41593767a33bd109
Sophie Brill Weitz <sophiebrillweitz@Sophies-MacBook-Pro.local>
2018-11-17 02:37:26 -0500

    trying to write to file
    Doesn't work yet, especially for the days, but it does create a file and put the classes and days (not the right days) in it.

--- getInput.java
+++ getInput.java
@@ -1,4 +1,6 @@
 import java.util.Arrays;
+import java.io.*;
+import java.lang.System.*;
 
 public class GetInput {
     public static String[] getInput() {
@@ -6,6 +8,7 @@ public class GetInput {
         boolean keepGoing = true;
         int i = 0;
         String[] className = new String[10];
+        String[] daysOfWeek = new String[7];
         while (keepGoing) {
             if (i == 0) {
                 System.out.println("Please input the name of your first class.");
@@ -14,7 +17,6 @@ public class GetInput {
             }
             className[i] = TextIO.getWord();
             boolean moreDays = true;
-            String[] daysOfWeek = new String[7];
             int j = 0;
             while (moreDays) {
                 System.out.println("Please enter the day of the week this class meets:");
@@ -27,8 +29,23 @@ public class GetInput {
             keepGoing = TextIO.getBoolean();
             i++;
         }
+        String classes=Arrays.toString(className);
+        String days = Arrays.toString(daysOfWeek);
+        writeFile(classes, days);
         return className;
-    }
+}
+    public static void writeFile(String classes, String days){
+      try {
+        FileWriter fw = new FileWriter("UserInfo.txt");
+        PrintWriter pw = new PrintWriter(fw);
+        pw.println("classes: "+classes);
+        pw.println("days: "+days);
+        pw.close();
+
+      } catch(IOException e){
+        System.out.println("error");
+      }
+  }
 
     public static void main(String[] args) {
         System.out.println(Arrays.toString(getInput()));
