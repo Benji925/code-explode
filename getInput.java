a33fc7b052217ec739c51f11d83c7efd79143013
Charlie Atchinson <atchinson.charlie@gmail.com>
2018-11-29 10:13:21 -0500

    This will  now write to a file without the nulls in the array, hopefully.
    I will add comments explaining each part next.

--- getInput.java
+++ getInput.java
@@ -2,18 +2,18 @@ import java.time.DayOfWeek;
 import java.util.Arrays;
 public class GetInput {
     public static void main(String[] args) {
-        public static String[] getInput() {
+        public static String[] getInput () {
             System.out.println("Hello User! This is the Code Explode Daily Planning App, or CEDPA");
             boolean keepGoing = true;
             String[] classes = new String[25];
             String[] timeStart = new String[25];
-            String[] Sunday = new String [25];
+            String[] Sunday = new String[25];
             String[] Monday = new String[25];
-            String[] Tuesday = new String [25];
+            String[] Tuesday = new String[25];
             String[] Wednesday = new String[25];
-            String[] Thursday = new String [25];
+            String[] Thursday = new String[25];
             String[] Friday = new String[25];
-            String[] Saturday = new String [25];
+            String[] Saturday = new String[25];
             int i = 0;
             boolean[] daysOfWeek = new boolean[7];
             String[] className = new String[10];
@@ -59,20 +59,24 @@ public class GetInput {
                     keepGoing = TextIO.getBoolean();
                     i++;
                 }
-                System.out.println(Arrays.toString(Sunday));
+                //TextIO.writeFile(java.lang.string classSchedule.txt);
+
+                /*System.out.println(Arrays.toString(Sunday));
                 System.out.println(Arrays.toString(Monday));
                 System.out.println(Arrays.toString(Tuesday));
                 System.out.println(Arrays.toString(Wednesday));
                 System.out.println(Arrays.toString(Thursday));
                 System.out.println(Arrays.toString(Friday));
-                System.out.println(Arrays.toString(Saturday));
+                System.out.println(Arrays.toString(Saturday)); */
+
+                ridofNull(Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday);
             }
-            public static String getClasses() {
+            public static String getClasses () {
                 System.out.println("What is the name of your class?");
                 String schoolClass = TextIO.getlnString();
                 return schoolClass;
             }
-            public static String AskDay() {
+            public static String AskDay () {
                 boolean right = false;
                 String day = " ";
                 while (!right) {
@@ -88,53 +92,105 @@ public class GetInput {
                 }
                 return day;
             }
-            public static boolean AnotherDay() {
+            public static boolean AnotherDay () {
                 System.out.println("Does the class meet another day?");
                 boolean meetAgain = TextIO.getlnBoolean();
                 return meetAgain;
             }
-            public static String PutInSunday(String[] classes, int i) {
+            public static String PutInSunday (String[]classes,int i){
                 String[] Sunday = new String[25];
                 Sunday[i] = classes[i];
                 return Sunday[i];
             }
-            public static String PutInMonday(String[] classes, int i) {
+            public static String PutInMonday (String[]classes,int i){
                 String[] Monday = new String[25];
                 Monday[i] = classes[i];
                 return Monday[i];
             }
-            public static String PutInTuesday(String[] classes, int i) {
+            public static String PutInTuesday (String[]classes,int i){
                 String[] Tuesday = new String[25];
                 Tuesday[i] = classes[i];
                 return Tuesday[i];
             }
-            public static String PutInWednesday(String[] classes, int i) {
+            public static String PutInWednesday (String[]classes,int i){
                 String[] Wednesday = new String[25];
                 Wednesday[i] = classes[i];
                 return Wednesday[i];
                 return className;
             }
-            public static String PutInThursday(String[] classes, int i) {
+            public static String PutInThursday (String[]classes,int i){
                 String[] Thursday = new String[25];
                 Thursday[i] = classes[i];
                 return Thursday[i];
             }
-            public static String PutInFriday(String[] classes, int i) {
+            public static String PutInFriday (String[]classes,int i){
                 String[] Friday = new String[25];
                 Friday[i] = classes[i];
                 return Friday[i];
             }
-            public static String PutInSaturday(String[] classes, int i) {
+            public static String PutInSaturday (String[]classes,int i){
                 String[] Saturday = new String[25];
                 Saturday[i] = classes[i];
                 return Saturday[i];
             }
-            public static String startTime() {
+            public static String startTime () {
                 System.out.println("What time does the class start?");
                 String start = TextIO.getlnString();
                 return start;
-                public static void main(String[] args) {
-                    System.out.println(Arrays.toString(getInput()));
+            }
+            public static void ridOfNull(Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday) {
+                int i = 0;
+                TextIO.writeFile(java.lang.String classSchedule.txt);
+                if(Sunday[i] != "null") {
+                    System.out.println(Sunday[i]);
+                    i++;
+                }else{
+                    i=0;
+                    break;
+                }
+                if(Monday[i] != "null") {
+                    System.out.println(Monday[i]);
+                    i++;
+                }else {
+                    i=0;
+                    break;
+                }
+                if(Tuesday[i] != "null") {
+                    System.out.println(Tuesday[i]);
+                    i++;
+                }else {
+                    i=0;
+                    break;
+                }
+                if(Wednesday[i] != "null") {
+                    System.out.println(Wednesday[i]);
+                    i++;
+                }else {
+                    i=0;
+                    break;
+                }
+                if(Thursday[i] != "null") {
+                    System.out.println(Thursday[i]);
+                    i++;
+                }else {
+                    i=0;
+                    break;
+                }
+                if(Friday[i] != "null") {
+                    System.out.println(Friday[i]);
+                    i++;
+                }else {
+                    i=0;
+                    break;
+                }
+                if(Saturday[i] != "null") {
+                    System.out.println(Saturday[i]);
+                    i++;
+                }else {
+                    i=0;
+                    break;
                 }
             }
         }
+    }
+}
