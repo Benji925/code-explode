89ad77a8e02f605dd86a33f4f57bc627fd8f0b4d
Ben Greene <bgreene@brandeis.edu>
2018-12-02 19:53:29 -0500

    Update ArrayMaker.java

--- ArrayMaker.java
+++ ArrayMaker.java
@@ -9,39 +9,61 @@ public class ArrayMaker{
   public static void main(String[] args){
     Scanner userInput = new Scanner(System.in);
     File fileName = new File("classes.txt");
-    String [][] schedule= new String[20][3];
+    int counter=0;
+    boolean continue=true;
+    try {
+      Scanner fileReader = new Scanner(fileName);
+      While(continue==true){
+        String line=fileReader.NextLine();
+        if(str != null && !str.isEmpty()){
+          continue=false;
+        }
+        else {
+          counter++;
+          System.out.println(counter);
+        }
+      }
+      System.out.println(counter);
+        fileReader.close();
+        }
 
+      catch(FileNotFoundException e){
+        System.out.println("Didn't find the file. Go to the library.");
+      }
+
+    String[][] schedule= new String[5][3];
 
     try {
       Scanner fileReader = new Scanner(fileName);
-      for(int i=0;i<schedule.length;i++){
-        while(fileReader.hasNext()){
+      //while(fileReader.hasNext()){
+        for(int i=0;i<schedule.length-1;i++){
           String day = fileReader.next();
           schedule[i][0]=day;
           String time = fileReader.next();
           schedule[i][1]=time;
           String class1= fileReader.next();
           schedule[i][2]=class1;
-          System.out.println(Arrays.toString(schedule));
-        }
 
+        }
+        System.out.println(Arrays.deepToString(schedule));
+        fileReader.close();
       }
-      fileReader.close();
-    }
+
+
     catch(FileNotFoundException e){
       System.out.println("Didn't find the file. Go to the library.");
     }
     int todaynum= Calendar.DAY_OF_WEEK;
     String date=whatday(todaynum);
-    for(int i=0; i<schedule.length; i++){
-      String classdate=schedule[i][0];
-      // if(classdate.equals(date)){
-      //   System.out.println(Arrays.toString(schedule[i]));
-      // }
+    for(int j=0; j<schedule.length; j++){
+      String classdate=schedule[j][0];
+      if(classdate.equals(date)){
+        System.out.println(Arrays.toString(schedule[j]));
+      }
       System.out.println(classdate);
-      // System.out.println(date);
     }
   }
+
   public static String whatday(int day){
     if(day==1){
       return "Mon";
@@ -66,4 +88,5 @@ public class ArrayMaker{
     }
     return "error!";
   }
+
 }
